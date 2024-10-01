0ff06ed930912112d0b9f6f2a8789002472af83f
Ehab Basha <99748749+EhabBen10@users.noreply.github.com>
2024-10-01 11:08:50 +0200

    adding a logging so power automate can read it

---
+++ CertificateUpdater.Infrastructure/AWSSimulators/LoggingToPowerAutomate.cs
@@ -0,0 +1,57 @@
+﻿namespace CertificateUpdater.Services.AWSSimulators;
+
+public class LoggingToPowerAutomate
+{
+	private readonly string _logFilePath;
+	public LoggingToPowerAutomate(string logFilePath)
+	{
+		_logFilePath = logFilePath;
+
+		EnsureDirectoryExists();
+		ClearLogFile();
+	}
+
+	public void Log(string message)
+	{
+		try
+		{
+			using (StreamWriter sw = new(_logFilePath, true))
+			{
+				sw.WriteLine(message);
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error writing to log file: {ex.Message}");
+		}
+	}
+	private void EnsureDirectoryExists()
+	{
+		if (!string.IsNullOrWhiteSpace(_logFilePath))
+		{
+			string? directory = Path.GetDirectoryName(_logFilePath);
+
+			// Ensure directory is valid (not null)
+			if (!string.IsNullOrWhiteSpace(directory) && !Directory.Exists(directory))
+			{
+				Directory.CreateDirectory(directory);
+			}
+		}
+
+	}
+	private void ClearLogFile()
+	{
+		try
+		{
+			if (File.Exists(_logFilePath))
+			{
+				// Overwrite the file with an empty string, clearing the content
+				File.WriteAllText(_logFilePath, string.Empty);
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"Error clearing log file: {ex.Message}");
+		}
+	}
+}
--- XLBygByggeBasenCertificateUpdater/Program.cs
+++ XLBygByggeBasenCertificateUpdater/Program.cs
@@ -17,6 +17,8 @@ IDateTimeProvider dateTimeProvider = new DateTimeProvider();
 ILogProvider logProvider = new LocalFileLogProvider(basePath + "AWSSimulator\\RunLog.txt", dateTimeProvider);
 ICredentialProvider credentialProvider = new LocalFileCredentialProvider(basePath + "AWSSimulator\\TunUserNr.txt", basePath + "AWSSimulator\\UserName.txt",
 	basePath + "AWSSimulator\\Password.txt", basePath + "AWSSimulator\\Aspect4Username.txt", basePath + "AWSSimulator\\Aspect4Password.txt");
+LoggingToPowerAutomate loggingToPowerAutomate = new LoggingToPowerAutomate(basePath + "AWSSimulator\\AutomateLog.txt");
+
 BaseSettings settings = new ByggeBasenSettings()
 {
 	BaseUrl = "http://services.byggebasen.dk/V3/BBService.svc/",
@@ -52,27 +54,33 @@ IHazardChangeCSVCreator hazardChangeCSVCreator = new HazardChangeCSVCreator();
 ICsvToXlsxConverter csvToXlsxConverter = new CsvToXlsxConverter();
 //IPostChangesService postChangesService = new PostChangesService(client, credentialProvider);
 Console.WriteLine("(1/5) Getting all changes");
+loggingToPowerAutomate.Log("Getting all changes");
 var productchangesResult = await getKatalogChangesService.GetProductChanges(default);
 if (productchangesResult.IsFailure)
 {
 	Console.WriteLine(productchangesResult.Error.Message);
+	loggingToPowerAutomate.Log(productchangesResult.Error.Message);
 	Thread.Sleep(10000);
 	return;
 }
 
 Console.WriteLine("(2/5) Getting all changed products");
+loggingToPowerAutomate.Log("(2/5) Getting all changed products");
 var productResult = await getProductBatchService.GetProductBatch(productchangesResult.Value, default);
 if (productResult.IsFailure)
 {
 	Console.WriteLine(productResult.Error.Message);
+	loggingToPowerAutomate.Log(productResult.Error.Message);
 	Thread.Sleep(10000);
 	return;
 }
 Console.WriteLine("(3/5) Finding changes to certifications, EPDs and Hazard sentences.");
+loggingToPowerAutomate.Log("(3/5) Finding changes to certifications, EPDs and Hazard sentences.");
 var certificationResult = certificationChangeFinder.FindCertificationChanges(productResult.Value);
 var epdResult = ePDChangeFinder.FindEPDChanges(productResult.Value);
 var hazardResult = hazardChangeFinder.FindHazardChanges(productResult.Value);
 Console.WriteLine("(4/5) Creating Csv Files");
+loggingToPowerAutomate.Log("(4/5) Creating Csv Files");
 var csvResult1 = cSVFileCreator.CreateCertificationChangeCSVFile
 	(basePath, certificationResult.ToList());
 var csvResult2 = epdChangeCSVCreator.CreateEPDCSVFile(basePath, epdResult.ToList());
@@ -86,6 +94,7 @@ ICollection<Result<string>> csvResult = new List<Result<string>>
 if (csvResult.Any(x => x.IsFailure))
 {
 	Console.WriteLine(csvResult.FirstOrDefault(x => x.IsFailure == true)!.Error.Message);
+	loggingToPowerAutomate.Log(csvResult.FirstOrDefault(x => x.IsFailure == true)!.Error.Message);
 	Thread.Sleep(20000);
 	return;
 }
@@ -96,8 +105,10 @@ foreach (var item in csvResult)
 }
 csvToXlsxConverter.ConvertToXlsx(basePath, csvResult4);
 Console.WriteLine("(5/5) Updating RunLog");
+loggingToPowerAutomate.Log("(5/5) Updating RunLog");
 logProvider.UpdateLog();
 Console.WriteLine("End");
+loggingToPowerAutomate.Log("End");
 Thread.Sleep(10000);
 //string basePath = "C:\\Users\\AME\\OneDrive - XL-BYG a.m.b.a\\Vigtige filer\\ByggebasenTest\\";
 //ICsvToXlsxConverter csvToXlsxConverter = new CsvToXlsxConverter();
